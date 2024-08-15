52c4b812907257fa5c3275732fe44f6c41a4b162
Mohamed Ben Makhlouf <benmakhlouf@softcatalyst.com>
2024-08-15 17:37:43 +0200

    update packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from "@angular/core";
 import { IonApp, IonRouterOutlet } from "@ionic/angular/standalone";
-import { HttpClientModule } from "@angular/common/http";
+import {} from "@angular/common/http";
 @Component({
 	selector: "app-root",
 	templateUrl: "app.component.html",
 	standalone: true,
-	imports: [IonApp, IonRouterOutlet, HttpClientModule],
+	imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-	constructor() {}
-}
+export class AppComponent {}
